7af380a1423d6f58939b07e68e83fa1248592838
Chen An <v-chenan1@microsoft.com>
2021-11-16 08:40:00 +0000

    提交

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -10,6 +10,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
     public class Program
